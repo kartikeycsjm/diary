5fb315cdbc84ffebf1107e20378de4e8d0ba652f
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-03-03 23:09:59 +0530

    added border in task component

--- app/components/Task.jsx
+++ app/components/Task.jsx
@@ -6,15 +6,16 @@ const Task = ({title,date,description,del}) => {
         flex justify-center items-center my-5'>
             <div className='my-[2%] w-[96%] min-h-[178px] rounded-lg bg-[#3333]
             flex items-center flex-col'>
-                <div id="head" className='w-full text-[15px] flex items-start py-2'>
-                    <h3 className='px-5 text-[20px] '>{title}</h3>
-                    <span className='text-[18px]'>{date}</span>
+                <div id="head" className='w-full min-h-[70px] text-[15px] border-b flex items-center justify-around py-2'>
+                    <h3 className='text-[18px] w-[40%]'>{title}</h3>
+                    <span className='text-[12px] w-[40%]'>{date}</span>
+                    <button className='flex justify-center items-center w-[30px] h-[30px] cursor-not-allowed rounded-full text-[30px] border-[3px] border-red-900' onClick={del}>-</button>
                 </div>
                 <div id="desc" 
                  className='text-green-white p-3 text-justify text-[13px]'>
                     <p className='min-h-[68px]'>{description}</p>
                 </div>
-                <button className='m-2 w-[50px] h-[35px] flex justify-center items-center cursor-not-allowed rounded-xl text-[30px] bg-red-900' onClick={del}>-</button>
+               
             </div>
         </div>
     )
